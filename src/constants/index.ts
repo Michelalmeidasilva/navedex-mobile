5357c94cfa10890adda25d73bcf2d210b0b58b2b
michel almeida <michel.almeida@nave.rs>
2021-06-22 11:59:33 -0300

    chore(renderNave): added render

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,2 @@
 export * from './images';
-export * from './auth';
 export * from './messages';
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -6,27 +6,25 @@ import styled from 'styled-components/native';
 
 const Home: FC = () => {
   return (
-    <ScrollView pagingEnabled showsHorizontalScrollIndicator={true}>
-      <Column pt='32px' pr='16px' pl='18px'>
-        <Row>
-          <Column flex={5}>
-            <Text width='50%' fontSize='22px' lineHeight='32px' fontWeight={600}>
-              Navers
-            </Text>
-          </Column>
+    <Column pt='32px' pr='16px' pl='18px'>
+      <Row>
+        <Column flex={5}>
+          <Text width='50%' fontSize='22px' lineHeight='32px' fontWeight={600}>
+            Navers
+          </Text>
+        </Column>
 
-          <Column flex={5}>
-            <Button
-              title='Adicionar Naver'
-              accessibilityLabel='Clique no botão para adicionar um naver'
-              variant='primary'
-            ></Button>
-          </Column>
-        </Row>
+        <Column flex={5}>
+          <Button
+            title='Adicionar Naver'
+            accessibilityLabel='Clique no botão para adicionar um naver'
+            variant='primary'
+          ></Button>
+        </Column>
+      </Row>
 
-        <NaversListView />
-      </Column>
-    </ScrollView>
+      <NaversListView />
+    </Column>
   );
 };
 
