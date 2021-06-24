d177b0b94e53d759962aaab6d79f1a3ca93792d5
michel almeida <michel.almeida@nave.rs>
2021-06-24 00:32:06 -0300

    chore(service-navers): added service for navers

--- src/components/Navers/NaverView.tsx
+++ src/components/Navers/NaverView.tsx
@@ -1,19 +1,27 @@
 import React, { FC, useState, useEffect } from 'react';
+<<<<<<< HEAD:src/components/Navers/NaverView.tsx
 
 import { ScrollView, View, FlatList } from 'react-native';
 import { Row, Column, Text, Button, Icon } from 'src/components';
+=======
+import { FlatList, ActivityIndicator } from 'react-native';
+>>>>>>> 524e563 (chore(service-navers): added service for navers):src/components/Navers/NaversList.tsx
 import styled from 'styled-components/native';
 
+import { Row, Column, Text, Icon, RowProps, Modal } from 'src/components';
+import { getNavers } from 'src/services';
 interface NaverData {
   id: any;
-  jobRole: string;
-  admissionDate: string;
-  birthdate: string;
-  project: string;
   name: string;
+  admissionDate: string;
+  job_role: string;
+  user_id: string;
+  project: string;
+  birthdate: string;
   url: string;
 }
 
+<<<<<<< HEAD:src/components/Navers/NaverView.tsx
 const NaversListView = () => {
   const [data, setData] = useState<NaverData[]>([
     {
@@ -55,12 +63,41 @@ const NaversListView = () => {
       url: 'https://nave.rs/static/eca192e3c9fee202452237f40b268a0d/2a4de/vic-naver.png'
     }
   ]);
+=======
+type NaversListComponentProps = RowProps;
 
-  useEffect(() => {}, [data]);
+const NaversListComponent: FC<NaversListComponentProps> = ({ ...props }) => {
+  const [data, setData] = useState<NaverData[]>([]);
+  const [isFetchingData, setIsFetchingData] = useState<boolean>(false);
+  const [isErrorFetching, setIsErrorFetching] = useState<boolean>(false);
+  const [messageModal, setMessageModal] = useState<string>('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsFetchingData(true);
+>>>>>>> 524e563 (chore(service-navers): added service for navers):src/components/Navers/NaversList.tsx
+
+        const navers = await getNavers();
+        setData(navers);
+      } catch (err) {
+        setMessageModal('Erro ao carregar os navers');
+        setIsErrorFetching(true);
+      } finally {
+        setIsFetchingData(false);
+      }
+    };
+    fetchData();
+  }, []);
 
   const renderNaver = ({ item }) => {
+<<<<<<< HEAD:src/components/Navers/NaverView.tsx
     const { id, name, jobRole, url } = item;
 
+=======
+    const { id, name, job_role: jobRole, url } = item;
+    console.log(jobRole.length);
+>>>>>>> 524e563 (chore(service-navers): added service for navers):src/components/Navers/NaversList.tsx
     return (
       <Column key={id} mt='28px'>
         <Image source={{ uri: url }} />
@@ -73,17 +110,25 @@ const NaversListView = () => {
             {jobRole}
           </Text>
 
+<<<<<<< HEAD:src/components/Navers/NaverView.tsx
           <Row mt='10px'>
             <Icon ml='5px' icon='trash'></Icon>
             <Icon ml='20px' icon='edit'></Icon>
           </Row>
         </Column>
+=======
+        <Row mt='10px'>
+          <Icon ml='5px' icon='trash' color='black' width={14} height={18}></Icon>
+
+          <Icon ml='24px' icon='edit' color='black' width={18.41} height={18.41}></Icon>
+        </Row>
+>>>>>>> 524e563 (chore(service-navers): added service for navers):src/components/Navers/NaversList.tsx
       </Column>
     );
   };
 
-  console.log(data);
   return (
+<<<<<<< HEAD:src/components/Navers/NaverView.tsx
     <Column>
       <FlatList
         columnWrapperStyle={{ justifyContent: 'space-around' }}
@@ -93,6 +138,31 @@ const NaversListView = () => {
         keyExtractor={item => item.id}
         renderItem={renderNaver}
       />
+=======
+    <Column {...props}>
+      {isFetchingData ? (
+        <Column mt='150px' justifyContent='center' alignItems='center'>
+          <ActivityIndicator size='large' color='#0000ff' />
+
+          <Text mt='10px'>Carregando os navers ...</Text>
+        </Column>
+      ) : (
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          initialNumToRender={4}
+          data={data}
+          numColumns={2}
+          keyExtractor={item => item.id}
+          renderItem={renderNaver}
+        />
+      )}
+
+      <Modal isVisible={isErrorFetching} handleClose={(): void => setIsErrorFetching(false)}>
+        <Column>
+          <Text color='red'>{messageModal}</Text>
+        </Column>
+      </Modal>
+>>>>>>> 524e563 (chore(service-navers): added service for navers):src/components/Navers/NaversList.tsx
     </Column>
   );
 };
