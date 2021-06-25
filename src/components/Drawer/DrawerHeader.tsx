338267ac57de2b61276fd8fb4b0bbbad0b6e33e1
michel almeida <michel.almeida@nave.rs>
2021-06-25 13:32:55 -0300

    chore(elevation): added elevation in drawer header

--- src/components/Drawer/DrawerHeader.tsx
+++ src/components/Drawer/DrawerHeader.tsx
@@ -3,7 +3,7 @@ import { useNavigation, DrawerActions } from '@react-navigation/native';
 import { TouchableOpacity, Image } from 'react-native';
 import styled from 'styled-components/native';
 
-import { Icon, RowProps, Row } from 'src/components';
+import { Icon, Row } from 'src/components';
 import { theme } from 'src/theme';
 import { IMAGES_URL } from 'src/constants';
 
@@ -12,7 +12,7 @@ const DrawerHeader: FC = () => {
   const { colors } = theme;
 
   return (
-    <RowContainer bg={colors.background}>
+    <Container backgroundColor={colors.background}>
       <Row flex={3}>
         <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
           <Icon icon='hamburguer' color={colors.black} width={35} height={14} pl={17} pt={25} />
@@ -22,13 +22,14 @@ const DrawerHeader: FC = () => {
       <Row flex={7}>
         <Image style={{ width: 156, height: 40, alignSelf: 'center' }} source={IMAGES_URL.logo} />
       </Row>
-    </RowContainer>
+    </Container>
   );
 };
 
-const RowContainer: FC<RowProps> = styled(Row)`
-  height: 64px;
-  box-shadow: 0px 2px 4px rgba(33, 33, 33, 0.25);
+const Container = styled.View`
+  background-color: ${props => props.backgroundColor};
+  elevation: 7;
+  height: 64;
+  flex-direction: row;
 `;
-
 export default DrawerHeader;
