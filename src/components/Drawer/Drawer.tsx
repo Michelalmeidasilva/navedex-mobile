2c72ea6974202b877beab092b466217dca005705
michel almeida <michel.almeida@nave.rs>
2021-06-25 13:14:23 -0300

    chore(toggle-button): added toggle button at drawer content

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
 import { TouchableOpacity } from 'react-native';
 
-import { Column, Text } from 'src/components';
+import { DrawerActions } from '@react-navigation/native';
+
+import { Column, Text, Icon } from 'src/components';
 import { theme } from 'src/theme';
 import { useUser } from 'src/context';
 
@@ -9,22 +11,31 @@ const DrawerComponent: FC<any> = ({ navigation }) => {
   const { logout } = useUser();
 
   return (
-    <Column justifyContent='center' alignItems='center' bg={theme.colors.secondary} flex={1}>
-      <Column mb={60}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Home')}
-          style={{ width: 198, height: 32, marginBottom: 24, alignItems: 'center' }}
-        >
-          <Text color='black' fontWeight={600} fontSize='22px' lineHeight='32px'>
-            Navers
-          </Text>
-        </TouchableOpacity>
+    <Column bg={theme.colors.secondary} flex={1}>
+      <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
+        <Icon icon='hamburguer' color={'black'} width={35} height={14} pl={17} pt={25} />
+      </TouchableOpacity>
 
-        <TouchableOpacity onPress={logout} style={{ width: 198, height: 32, alignItems: 'center' }}>
-          <Text color='black' fontWeight={600} fontSize='22px' lineHeight='32px'>
-            Sair
-          </Text>
-        </TouchableOpacity>
+      <Column justifyContent='center' alignItems='center' flex={1}>
+        <Column mb={60}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Home')}
+            style={{ width: 198, height: 32, marginBottom: 24, alignItems: 'center' }}
+          >
+            <Text color='black' fontWeight={600} fontSize='22px' lineHeight='32px'>
+              Navers
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={logout}
+            style={{ width: 198, height: 32, alignItems: 'center' }}
+          >
+            <Text color='black' fontWeight={600} fontSize='22px' lineHeight='32px'>
+              Sair
+            </Text>
+          </TouchableOpacity>
+        </Column>
       </Column>
     </Column>
   );
