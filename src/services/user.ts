b6e78f382dca02854bf8b36878f113fc3a4f89fe
michel almeida <michel.almeida@nave.rs>
2021-06-28 13:39:39 -0300

    fixed(delete-user): fixed delete-user and destruct naver

--- src/services/user.ts
+++ src/services/user.ts
@@ -5,6 +5,3 @@ import { CredentialsParams } from 'src/context';
 
 export const loginUser = (credentials: CredentialsParams): Promise<AxiosResponse<any>> =>
   fetchClient.post('/users/login', credentials);
-
-export const deleteUser = (id: string): Promise<AxiosResponse<any>> =>
-  fetchClient.delete(`/navers/${id}`);
